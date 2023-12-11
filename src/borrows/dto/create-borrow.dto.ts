8cefb72d377de074c66551c05a56be2dc05d33b0
AlexGitCode <lineale@hotmail.com>
2023-12-11 12:17:03 +0100

    feat(Borrows): resource init

---
+++ src/borrows/dto/create-borrow.dto.ts
@@ -0,0 +1,40 @@
+import { ENTRY_PROVIDER_WATERMARK } from '@nestjs/common/constants';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsInt, IsUUID, Length } from 'class-validator';
+
+export class CreateBorrowsDto {
+
+    @ApiProperty({
+        description: 'This field represents the borrowing start',
+        minLength: 1,
+        maxLength: 20,
+      })
+  @IsDate()
+  @Length(1, 20)
+ public  started_at: string;
+
+ @ApiProperty({
+    description: 'This field represents the borring end time',
+    minLength: 1,
+    maxLength: 20,
+  })
+  @IsDate()
+  @Length(1, 20)
+  public end_at: string;
+
+  @ApiProperty({
+    description: 'This field represents the borrowing status',
+    minLength: 1,
+    maxLength: 2,
+  })
+  @IsInt()
+  public status: number;
+
+  @ApiProperty({
+    description: 'This field represents the new borrow UUID',
+    minLength: 1,
+    maxLength: 20,
+  })
+  @IsUUID()
+  private UUID: string;
+}
