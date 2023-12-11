494c626c523d29f485945c5ceb6c5f212e0b1155
tmnpierre <tomyn.pierre@gmail.com>
2023-12-11 12:14:35 +0100

    feat(books): update create-book.dto.ts

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,31 @@
-export class CreateBookDto {}
+import { IsString, IsUUID, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBookDto {
+  @ApiProperty({
+    description: 'This Field represents the book UUID',
+    minLength: 36,
+    maxLength: 36,
+  })
+  @IsUUID()
+  @Length(36, 36)
+  	private book_UUID:'string';
+
+  @ApiProperty({
+    description: "This Field represents the book's name",
+    minLength: 2,
+    maxLength: 40,
+    })
+  @IsString()
+  @Length(2, 40)
+  	public name: 'string';
+
+  @ApiProperty({
+    description: "This Field represents the book's description",
+    minLength: 2,
+    maxLength: 500,
+    })
+  @IsString()
+  @Length(2, 500)
+  	public description: 'string';
+}
