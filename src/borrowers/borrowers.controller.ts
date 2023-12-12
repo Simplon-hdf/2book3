6e27f4f6dcb5a93a0098660dc8fed0210673e656
AlexGitCode <lineale@hotmail.com>
2023-12-12 16:11:54 +0100

    feat(): init controller resource

--- src/borrowers/borrowers.controller.ts
+++ src/borrowers/borrowers.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, , Param, Delete, Patch } from '@nestjs/common';
 import { BorrowersService } from './borrowers.service';
 import { CreateBorrowerDto } from './dto/create-borrower.dto';
 import { UpdateBorrowerDto } from './dto/update-borrower.dto';
+import { CreateHumanInformationDto } from 'src/human-informations/dto/create-human-information.dto';
+import { UpdateHumanInformationDto } from 'src/human-informations/dto/update-human-information.dto';
 
 @Controller('borrowers')
 export class BorrowersController {
   constructor(private readonly borrowersService: BorrowersService) {}
 
   @Post()
-  create(@Body() createBorrowerDto: CreateBorrowerDto) {
-    return this.borrowersService.create(createBorrowerDto);
+  public create(@Body() createBorrowerDto: CreateBorrowerDto, createHumanInformationDto: CreateHumanInformationDto) {
+    return this.borrowersService.create(createBorrowerDto, createHumanInformationDto);
   }
 
   @Get()
-  findAll() {
-    return this.borrowersService.findAll();
+  public getByUUID(
+    @Param('uuid') UUID: string ){
+    return this.borrowersService.getByUUID(UUID);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.borrowersService.findOne(+id);
+
+  @Patch()
+  public updateByUUID(
+      @Body() updateBorrowerDto: UpdateBorrowerDto, updateHumanInformationDto : UpdateHumanInformationDto) {
+    return this.borrowersService.updateByUUID(updateBorrowerDto, updateHumanInformationDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBorrowerDto: UpdateBorrowerDto) {
-    return this.borrowersService.update(+id, updateBorrowerDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.borrowersService.remove(+id);
+
+  @Delete(':uuid')
+  remove(@Param('uuid') UUID: string) {
+    return this.borrowersService.deleteByUUID(UUID);
   }
 }
