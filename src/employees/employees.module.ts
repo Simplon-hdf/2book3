f322a84958176bfa7db1c9062f232e2a48e2fa8b
tmnpierre <tomyn.pierre@gmail.com>
2023-12-12 21:47:27 +0100

    feat(core): update all module

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employees.service';
 import { EmployeesController } from './employees.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [EmployeesController],
-  providers: [EmployeeService],
+  providers: [EmployeeService, PrismaService],
 })
 export class EmployeesModule {}
