b6acf569beefc91ebd327b85594f46899092ed5f
Azzedine Ajgoun <azzedineajgoun@gmail.com>
2023-12-12 13:42:41 +0100

    feat(authors): init service and normalization of responses.

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -2,30 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { PrismaService } from 'src/prisma.service';
+import NormalizedResponse from '../utils/normalized.response';
 
 @Injectable()
 export class AuthorsService {
   constructor(private readonly prisma: PrismaService) {}
 
   public async create(createAuthorDto: CreateAuthorDto) {
-    return await this.prisma.authors.create({
+    
+    const createdAuthor = await this.prisma.authors.create({
       data: {
         firstname: createAuthorDto.firstName,
         lastname: createAuthorDto.lastName,
       },
     });
+    const createdMessage = "Author has been created";
+
+    return new NormalizedResponse(createdMessage,createdAuthor).toJSON()
   }
 
   public async getByUUID(uuid: string) {
-    return await this.prisma.authors.getByUUID({
+    
+    const gettedAuthor = await this.prisma.authors.getByUUID({
       where: {
         UUID: uuid,
       },
     });
+    const gettedMessage = `Author has been found`; 
+
+    return new NormalizedResponse(gettedMessage,gettedAuthor).toJSON();
   }
 
   public async updateByUUID(uuid: string, updateAuthorDto: UpdateAuthorDto) {
-    return await this.prisma.authors.updateByUUID({
+    
+    const updatedAuthor = await this.prisma.authors.updateByUUID({
       where: {
         UUID: uuid,
       },
@@ -34,14 +44,21 @@ export class AuthorsService {
         lastname: updateAuthorDto.lastName,
       }
     });
+    const updatedMessage = `The author has been updated`;
+    
+    return new NormalizedResponse(updatedMessage,updatedAuthor);
   }
 
   public async deleteByUUID(uuid: string) {
-    return await this.prisma.authors.deleteByUUID({
+    
+    const deletedAuthor = await this.prisma.authors.deleteByUUID({
       where: {
         UUID: uuid,
       },
     });
+    const deletedMessage = `The author has been deleted`;
+    
+    return new NormalizedResponse(deletedMessage,deletedAuthor);
   }
 }
 
