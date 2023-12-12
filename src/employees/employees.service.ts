88669050b65047afe2ae65ea64c56aca7ef458c6
AlexGitCode <lineale@hotmail.com>
2023-12-12 13:27:52 +0100

    feat(Borrowers): update service resource

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { PrismaService } from 'src/prisma.service';
+import NormalizedResponse from 'src/utils/normalized.response';
+import { CreateHumanInformationDto } from 'src/human-informations/dto/create-human-information.dto';
+
+
 
 @Injectable()
-export class EmployeesService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
-  }
-
-  findAll() {
-    return `This action returns all employees`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} employee`;
-  }
-
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return `This action updates a #${id} employee`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} employee`;
-  }
+export class EmployeeService {
+  constructor(private readonly prisma: PrismaService) {}
 }
+
