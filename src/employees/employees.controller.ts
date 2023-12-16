ced3184d0157db47a75d408a543294810740e034
tmnpierre <tomyn.pierre@gmail.com>
2023-12-16 21:47:59 +0100

    feat(employees): update employees.controller.ts

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,32 +1,35 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { CreateHumanInformationDto } from '../human-informations/dto/create-human-information.dto';
 import { UpdateHumanInformationDto } from '../human-informations/dto/update-human-information.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
+@ApiTags('Employees')
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create an employee', description: 'Add a new employee along with their human information to the database' })
   public async create(@Body() createEmployeeDto: CreateEmployeeDto, @Body() CreateHumanInformationDto: CreateHumanInformationDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get(':uuid')
+  @ApiOperation({ summary: 'Get employee by UUID', description: 'Retrieve an employee using their UUID' })
   public async getByUUID(@Param('uuid') uuid: string) {
     return this.employeesService.getByUUID(uuid);
   }
 
   @Patch(':uuid')
-  updateByUUID(@Param('uuid') uuid: string,
-       @Body() updateHumanInformationDto: UpdateHumanInformationDto,
-  ) {
+  @ApiOperation({ summary: 'Update employee information', description: 'Update an employee\'s information using their UUID' })
+  updateByUUID(@Param('uuid') uuid: string, @Body() updateHumanInformationDto: UpdateHumanInformationDto) {
     return this.employeesService.updateByUUID(uuid, updateHumanInformationDto);
   }
 
   @Delete(':uuid')
+  @ApiOperation({ summary: 'Delete an employee', description: 'Remove an employee from the database using their UUID' })
   public async deleteByUUID(@Param('uuid') uuid: string) {
     return this.employeesService.deleteByUUID(uuid);
   }
