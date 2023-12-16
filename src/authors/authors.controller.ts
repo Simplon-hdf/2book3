82e57a5992c06f679e9f5493d1f23e1dddf62536
tmnpierre <tomyn.pierre@gmail.com>
2023-12-16 21:26:14 +0100

    feat(authors): update authors.controller.ts

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,30 +1,35 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
-import { CreateHumanInformationDto } from '../human-informations/dto/create-human-information.dto';
 import { UpdateHumanInformationDto } from '../human-informations/dto/update-human-information.dto';
 
+
 @Controller('authors')
+@ApiTags('Authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new author', description: 'Add a new author to the database' })
   create(@Body() createAuthorDto: CreateAuthorDto) {
     return this.authorsService.create(createAuthorDto);
   }
 
   @Get(':uuid')
-  getByUUID(
-    @Param('uuid') uuid: string) {
+  @ApiOperation({ summary: 'Get author by UUID', description: 'Retrieve an author using their UUID' })
+  getByUUID(@Param('uuid') uuid: string) {
     return this.authorsService.getByUUID(uuid);
   }
 
   @Patch(':uuid')
+  @ApiOperation({ summary: 'Update author information', description: 'Update an author\'s information by their UUID' })
   updateByUUID(@Param('uuid') uuid: string, @Body() updateHumanInformationDto: UpdateHumanInformationDto) {
     return this.authorsService.updateByUUID(uuid, updateHumanInformationDto);
   }
 
   @Delete(':uuid')
+  @ApiOperation({ summary: 'Delete an author', description: 'Remove an author from the database using their UUID' })
   deleteByUUID(@Param('uuid') uuid: string) {
     return this.authorsService.deleteByUUID(uuid);
   }
